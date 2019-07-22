b5576c8d08224ff5475ac677d630c0fccca824c1
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2019-07-22 09:24:38 -0400

    [ADJUST+ADD] Add readme and adjust the provider
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Via Composer
+
+```bash
+$ composer require walternascimentobarroso/laravel-crud-generator 
+```
+
+Add the service provider in `config/app.php`:
+
+```php
+WalterNascimentoBarroso\CrudGenerator\CrudGeneratorServiceProvider::class,
+```
+
--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -6,11 +6,21 @@ use Illuminate\Support\ServiceProvider;
 
 class CrudGeneratorServiceProvider extends ServiceProvider
 {
+    protected $commands = [
+        'WalterNascimentoBarroso\CrudGenerator\Console\Commands\CommonCrud',
+        'WalterNascimentoBarroso\CrudGenerator\Console\Commands\CrudInverse',
+        'WalterNascimentoBarroso\CrudGenerator\Console\Commands\CrudMigration',
+        'WalterNascimentoBarroso\CrudGenerator\Console\Commands\CrudRest',
+        'WalterNascimentoBarroso\CrudGenerator\Console\Commands\DbSettings',
+    ];
+
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
+
     public function register()
     {
+        $this->commands($this->commands);
     }
 }
