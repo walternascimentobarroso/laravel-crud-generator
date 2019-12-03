39db12821ba1d8fb90e4016fb73485c257f61744
walternascimentobarroso <walter.nascimento.barroso@gmail.com>
2019-12-03 11:26:29 -0400

    [DEPLOY]
    Signed-off-by: walternascimentobarroso <walter.nascimento.barroso@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e # termina o script com um código diferente de 0 se alguma coisa falhar
 # pull requests e commits para outras branches diferentes da master
 # não devem fazer o deploy, isso é opcional caso queira deletar as próximas 6 linhas
 # fique a vontade
-SOURCE_BRANCH="master"
+SOURCE_BRANCH="develop"
 
 # entre na pasta onde está o build do seu projeto e inicie um novo repositório git
 cd packages/WalterNascimentoBarroso/CrudGenerator
